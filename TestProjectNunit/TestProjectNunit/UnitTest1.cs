90c5f895f4a48eb30b6d68f8197a4fc11627dba4
Гуркин Павел Андреевич <gurkin.200389@gmail.com>
2024-03-02 10:46:46 +0500

    Первый комит

---
+++ TestProjectNunit/TestProjectNunit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestProjectNunit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
